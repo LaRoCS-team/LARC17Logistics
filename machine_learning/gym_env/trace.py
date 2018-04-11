c538ecc215bab9967fdd98d89b9a0d343c8b55d2
Samuel <samuel.chenatti@gmail.com>
2018-04-11 16:01:28 -0300

    Machine learning branch first commit

---
+++ machine_learning/gym_env/trace.py
@@ -0,0 +1,15 @@
+import matplotlib,sys
+matplotlib.rcParams["backend"] = "TkAgg"
+import csv
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+f = open("reward_history.csv")
+reader = csv.reader(f, delimiter=",")
+
+r = [float(row[0]) for row in reader]
+
+print("Avg: %s" %(sum(r)/len(r)))
+
+plt.plot(range(len(r)), r)
+plt.show()
