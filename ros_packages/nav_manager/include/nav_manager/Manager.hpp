992b21d53c185f9a0987bccb398d5ad0725e186c
thalesmro <thalesteng@gmail.com>
2018-04-12 16:49:41 -0300

    fixed build version. Started working on actual code

---
+++ ros_packages/nav_manager/include/nav_manager/Manager.hpp
@@ -0,0 +1,36 @@
+#ifndef Manager_H
+#define Manager_H
+#pragma once
+
+#include <ros/ros.h>
+#include <cmath>
+#include <std_msgs/Bool.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/PointCloud.h>
+#include <actionlib_msgs/GoalID.h>
+#include <actionlib_msgs/GoalStatusArray.h>
+
+using namespace std;
+
+
+class Manager {
+
+public:
+	Manager();
+	~Manager();
+
+
+private:
+	ros::NodeHandle nh;
+actionlib::SimpleActionServer<nav_manager::GoalAction> as_;
+	ros::Subscriber dist_sensors_sub;
+	ros::Publisher goToGoal, cancelGoal;
+		
+	void distSensorsCallback(const sensor_msgs::PointCloud::COnstPtr& msg);
+
+};
+
+#endif
+
+
+
